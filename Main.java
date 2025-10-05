6a3cdf1a616a68e09a1909403dabf52cb0c1475c
mohammadiriaz <mohammadr95@nycstudents.net>
2025-10-05 16:33:23 -0400

    Update Main.java
    Added the test cases

--- Main.java
+++ Main.java
@@ -2,9 +2,22 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Feeder f =  new Feeder(250);
+        Feeder f = new Feeder(500);
+        System.out.println(f);
+        System.out.println("Current food: " + f.getCurrentFood());
+        f.simulateOneDay(12);
+        System.out.println("Current food: " + f.getCurrentFood());
+        f = new Feeder(1000);
+        f.simulateOneDay(22);
+        System.out.println("Current food: " + f.getCurrentFood());
+        f = new Feeder(100);
+        f.simulateOneDay(5);
+        System.out.println("Current food: " + f.getCurrentFood());
+        f = new Feeder(2400);
+        System.out.println(f.simulateManyDays(10, 4) + " days");
+        f = new Feeder(250);
         System.out.println(f.simulateManyDays(10, 5) + " day(s)");
         f = new Feeder(0);
-        System.out.println(f.simulateManyDays(5, 10) + " day(s)");
+        System.out.println(f.simulateManyDays(5,10) + " day(s)");
     }
 }
